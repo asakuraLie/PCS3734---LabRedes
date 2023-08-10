1cebd207f92772aa65bee5ed7b7c0061ad4c9a76
asakuraLie <lie.asakura000@gmail.com>
2023-08-10 15:08:46 -0300

    ✨ I wannna die

--- react-native-redes/app/index.js
+++ react-native-redes/app/index.js
@@ -1,11 +1,22 @@
-import { View, Text } from "react-native";
+import { useState } from "react";
+import { View, Text, ScrollView, SafeAreaView } from "react-native";
+import { Stack, useRouter } from "expo-router";
+
+import { COLORS } from "../constants";
 
 const Home = () => {
-  return (
-    <View>
-      <Text>Home</Text>
-    </View>
-  );
+    const router = useRouter();
+
+    return (
+        <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+            <Stack.Screen
+                options={{
+                    headerStyle: { backgroundColor: COLORS.lightWhite },
+                    headerShadowVisible: false,
+                }}
+            />
+        </SafeAreaView>
+    );
 };
 
 export default Home;
