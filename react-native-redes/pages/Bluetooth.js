a24cfc05b4ffdce295f693da18866315f61894dc
asakuraLie <lie.asakura000@gmail.com>
2023-08-10 15:34:48 -0300

    🤡 AAAAA

---
+++ react-native-redes/pages/Bluetooth.js
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function Bluetooth() {
+    return (
+        <View style={{ flex: 1, backgroundColor: "blue" }}>
+            <Text>Bluetooth</Text>
+        </View>
+    );
+}
